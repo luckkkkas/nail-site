062c6baf5bd032cc43937809894ddede563d69fd
luckkkkas <lucashbo321@gmail.com>
2024-06-23 08:15:08 -0300

    visual alterado

--- src/script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById('botao');
-const menu = document.getElementById('esconder')
-
-botao.addEventListener('click',() => {
-    menu.classList.toggle('hidden');
-});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'custom-light-purple': '#dedce9',
+        'cusom-green-whats': '#33d951',
+      }
+    },
   },
   plugins: [],
 }
